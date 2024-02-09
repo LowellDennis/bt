c1edcc7d34d074c42b95f24321c05727906735d9
Lowell Dennis <lowell.dennis@hpe.com>
2024-02-08 18:07:03 -0600

    Updated create function

--- create/create.py
+++ create/create.py
@@ -11,39 +11,44 @@ from error    import ErrorMessage, UsageError
 from cmdline  import ParseCommandLine
 from postbios import PostBIOS
 from run      import DoCommand
-from vcs      import GetVCSInfo, UndefinedRepo
+from vcs      import GetVCSInfo, DoesBranchExist
 
 # Global variables
-Prms = None
-Opts = None
-Repo = None
-Work = None
-
-# Create a git worktree command
-# returns git worktree command string
-def WorktreeCommand():
-  global Opts, Prms, Repo, Work
-  # Generate command line, format = git worktree add [-b Branch] [<path>\]Name [<Commmit>]
-  command = 'git worktree add '
-  if Opts['branch']: command += '-b {0} '.format(Opts['branch'])
-  Work    = os.path.join(os.path.dirname(Repo), Prms[0])
-  command += '{0} '.format(Work)
-  command += Opts['commit'] if Opts['commit'] else 'HEAD'
-  return command
+Prms      = None
+Opts      = None
+Repo      = None
+Branch    = None
+Worktree  = None
+Abstree   = None
+Commitish = 'HEAD'
 
 # Clean command handler
 # returns 0 on success, DOES NOT RETURN otherwise
 def create():
-  global Opts, Prms, Repo, Work
+  global Opts, Prms, Repo, Branch, Worktree, Abstree, Commitish
 
   # Get command line information
-  Prms, Opts = ParseCommandLine({'repo': True, 'commit': True, 'branch': True}, 4)
+  Prms, Opts = ParseCommandLine({'repo': True}, 3)
   # DOES NOT RETURN if invalid options or parameters are found
 
-  # Make sure the worktree (name or path) is given
+  # Make sure required parameters are given
   argc = len(Prms)
-  if argc < 1:
-    UsageError('worktree path not given')
+  if argc < 2:
+    UsageError('worktree directory not given')
+    # DOES NOT RETURN
+  elif argc < 1:
+    UsageError('worktree branch not given')
+    # DOES NOT RETURN
+
+  # Get parameters (with defaults)
+  Branch    = Prms[0]
+  Worktree  = Prms[1]
+  Abstree   = os.path.abspath(Worktree)
+  Commitish = "HEAD" if len(Prms) < 3 else Prms[2]
+
+  # Make sure target directory does not exist
+  if os.path.exists(Abstree):
+    ErrorMessage('Directory already exists: {0}'.format(Worktree))
     # DOES NOT RETURN
 
   # Make sure repository exits
@@ -55,31 +60,38 @@ def create():
 
   # Get repo information
   info = GetVCSInfo(data.gbl.repos, Repo, 'repository', False)
-  if info:
-
-    # Make sure repo is a git repo
-    if info._VCSInfo__vcs._VCS__name != 'git':
-      ErrorMessage('Worktrees are only supported by git')
-      # DOES NOT RETURN
-
-    # Make sure worktree does not already exist
-    if (GetVCSInfo(data.gbl.worktrees, Prms[0], 'worktree', False)):
-      ErrorMessage('Worktree already exists: {0}'.format(Prms[0]))
-      # DOES NOT RETURN
-
-    # Get command for creating worktree
-    cmd = WorktreeCommand()
-    # Perform create operation
-    rc = DoCommand('Creating worktree', 'Create Worktree', cmd, info.VCS().Base())
-    if rc:
-      ErrorMessage('Unable to create worktree')
-      # DOES NOT RETURN
-
-    # Switch to newly created worktree
-    PostBIOS(['cd {0}'.format(Work)])
-  else:
-    ErrorMessage('Unable to get repo information')
+  if not info:
+    ErrorMessage('Not a repository: {0}'.format(Repo))
     # DOES NOT RETURN
+
+  # Make sure repo is a git repo
+  if info.VCS().Name() != 'git':
+    ErrorMessage('Not a git repository: {0}'.format(Repo))
+    # DOES NOT RETURN
+
+  # Make sure branch does not already exist
+  if DoesBranchExist(Repo, Branch):
+    ErrorMessage('Branch already exists: {0}'.format(Branch))
+    # DOES NOT RETURN
+
+  # Make sure worktree does not already exist
+  if (GetVCSInfo(data.gbl.worktrees, Abstree, 'worktree', False)):
+    ErrorMessage('Worktree already exists: {0}'.format(Worktree))
+    # DOES NOT RETURN
+
+  # Get command for creating worktree
+  cmd  = 'git worktree add -b {0} {1} {2}'.format(Branch, Abstree, Commitish)
+  # Perform create operation
+  rc = DoCommand('Creating worktree', 'Create Worktree', cmd, Repo)
+  if rc:
+    ErrorMessage('Unable to create worktree')
+    # DOES NOT RETURN
+
+  # Save resulting branch for destroy
+  data.lcl.SetItem('branch', Branch)
+
+  # Switch to newly created worktree
+  PostBIOS([Abstree[0:2], 'cd {0}'.format(Abstree)])
 
   # Return results
   return rc
