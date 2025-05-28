b39cbe93dcbea4e54827b0dceb39917765337a83
Lowell Dennis <lowell.dennis@hpe.com>
2025-05-28 16:25:57 -0500

    Prep for next release of tool

--- bt.py
+++ bt.py
@@ -13,7 +13,7 @@ from error    import NotAWorktree
 # If running this file
 if __name__ == '__main__':
   try:
-    print('BIOS Tool V0.2')
+    print('BIOS Tool V0.3')
     InitializeSettings()
     LoadCommands()
     DispatchCommand()
