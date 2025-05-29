9430f1fa3c695a73f752ee52e157415e94225906
Lowell Dennis <lowell.dennis@hpe.com>
2025-05-28 19:53:36 -0500

    Pull now only works for git repos and worktrees

--- pull/pull.py
+++ pull/pull.py
@@ -15,7 +15,7 @@ from vcs      import GetVCSInfo
 # returns 0 on success, DOES NOT RETURN otherwise
 def pull():
   # Get command line information
-  prms, opts = ParseCommandLine({'merge': True}, 1)
+  prms, opts = ParseCommandLine(None, 1)
   # DOES NOT RETURN if invalid options or parameters are found
 
   # Get partial name of target repositoy or worktree to be pulled
@@ -29,20 +29,13 @@ def pull():
     ErrorMessage('Could not find {0} in list of repositories and worktrees'.format(tgt))
     # DOES NOT RETURN
 
-  # Handle merge case
-  if opts['merge']:
-    # Make sure target is a worktree
-    if info.IsRepo():
-      ErrorMessage('Merge operation requires a worktree')
-      # DOES NOT RETURN
-
-    # Perform merge operation
-    cmd = 'git merge {0}'.format(opts['merge'])
-    rc = DoCommand('Merging changes from parent repository', 'Merge Operation', cmd, info.Repo())
-
-  else:
-    # Perform pull operation
-    cmd = 'git pull --rebase' if info.VCS()._VCS__name == 'git' else 'svn update'
-    rc = DoCommand('Syncing with upstream repository', 'Pull Operation', cmd, info.Repo())
+  # Pull operation is not available with SVN
+  if info._VCSInfo__vcs._VCS__name == "svn":
+    ErrorMessage('Pull operation cannot be performed on an SVN repository (use "bt upodate")')
+    # DOES NOT RETURN
+    
+  # Perform pull operation
+  cmd = 'git pull --rebase'
+  rc = DoCommand('Syncing with upstream repository', 'Pull Operation', cmd, info.Repo())
 
   return rc
