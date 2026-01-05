a846b4b047a66b16d128f936b603c35310fd53f3
Lowell Dennis <Lowell.Dennis@hpe.com>
2026-01-05 13:39:02 -0600

    Update version to V0.5 in bt.py

--- bt.py
+++ bt.py
@@ -13,7 +13,7 @@ from error    import NotAWorktree
 # If running this file
 if __name__ == '__main__':
   try:
-    print('BIOS Tool V0.4')
+    print('BIOS Tool V0.5')
     InitializeSettings()
     LoadCommands()
     DispatchCommand()
